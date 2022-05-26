72190e8b590d64efca93f6aa2751a1a5e64ca3ba
andyK0p <akoptev@gmail.com>
2022-05-26 19:26:36 +0300

    added basic auth with roles

--- spring-security-demo/src/main/java/ru/spring/springsecuritydemo/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/ru/spring/springsecuritydemo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.spring.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.spring.springsecuritydemo.model.Role;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+            .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
+            .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
     }
 
     @Bean
@@ -26,7 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
+                .roles(Role.ADMIN.name())
+                .build(),
+            User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles(Role.USER.name())
                 .build()
         );
     }
