087389b072c97cefb27d7faf2f4445ac7554cb77
andyK0p <44091296+andyk0p@users.noreply.github.com>
2022-05-26 17:26:44 +0300

    Init commit

---
+++ spring-security-demo/src/main/java/ru/spring/springsecuritydemo/model/Developer.java
@@ -0,0 +1,12 @@
+package ru.spring.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
